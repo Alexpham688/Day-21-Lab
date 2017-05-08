203fae5bb1a93e90777732d02b021d0f4fbf1443
Marcell Hayes <marcell.made@gmail.com>
2017-05-08 14:20:11 -0400

    routes created

--- pinCtrl.js
+++ pinCtrl.js
@@ -3,20 +3,33 @@ var app = angular.module('comboApp');
 
 app.controller('pinCtrl', function($scope, $http) {
     
-
+    console.log('test');
+    
     $http({
+//        method: 'GET',
+//        url: "https://ismaelc-pinterest.p.mashape.com/cellhaze/boards",
+//        headers: {
+//            'X-Mashape-Key'': 'LFaT0hiXenmshmpUhHJQ9Xb30xvAp1pvB1qjsnW9uKxVrNLH9J',
+//            'Accept': 'application/json'
+//        }
+//    }).then(function successfulCallback(response) {
+//        console.log(response);
+//    }, function(error) {
+//        console.log(error);
+//    });
+
         method: 'GET',
-        url: "https://ismaelc-pinterest.p.mashape.com/cellhaze/boards",
+        url: 'https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.',
         headers: {
-            'Basic X-Mashape-Key': 'LFaT0hiXenmshmpUhHJQ9Xb30xvAp1pvB1qjsnW9uKxVrNLH9J',
-            'Accept': 'application/json'
+            'X-Mashape-Key': 'udLszi6d52mshWMsXgiMvyf7si9Vp1Tg6GPjsndahqJSbycTSY',
+            Accept: 'text/plain'
         }
     }).then(function successfulCallback(response) {
+        
         console.log(response);
     }, function(error) {
         console.log(error);
     });
   
-
-
 })
+
--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var app = angular.module('comboApp', ['ngRoute']);
 
-app.controller('pinCtrl', function($scope, $http) {
-     
+app.config(function($routeProvider) {
+    $routeProvider.when('/pin', {
+        templateUrl: "pinView.html",
+        controller: "pinCtrl"
+    });
+    $routeProvider.when('/lab', {
+        templateUrl: "labView.html",
+        controller: "labCtrl"
+    });   
 });

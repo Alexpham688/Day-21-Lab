1cf365e410d566709fe64b5996eb7362bf8013ed
Mark Upson <markaupson@gmail.com>
2017-05-08 16:19:09 -0400

    fizz buzz is working!

--- labCtrl.js
+++ labCtrl.js
@@ -1,7 +1,12 @@
 var app = angular.module('comboApp');
 
-app.controller('labCtrl', function($scope){
+app.controller('labCtrl', function($scope, fizzBuzzService){
 
-    $scope
+  $scope.doFizzBuzz = function(input){
+
+    $scope.result = fizzBuzzService.getFizzBuzz(input);
+    console.log($scope.result);
+
+  };
 
 });
--- service.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('comboApp');
-
-app.factory('myFactory', function($scope){
-
-// var results=[];
-
-
-
-});
